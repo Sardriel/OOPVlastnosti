c75349edbe20120b29150fdb2ef58a5ba3d91a4b
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-12 16:50:30 +0100

    Add project files.

---
+++ OOPVlastnosti/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OOPVlastnosti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student novyStudent = new Student("Luboš Krejčí", true, 29);
+            Console.WriteLine(novyStudent);
+        }
+    }
+}
---
+++ OOPVlastnosti/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPVlastnosti
+{
+    class Student
+    {
+        public string jmeno;
+        public bool muz;
+        public int vek;
+        public bool plnolety;
+
+        public Student(string jmeno, bool pohlavi, int vek)
+        {
+            this.jmeno = jmeno;
+            this.muz = pohlavi;
+            this.vek = vek;
+            plnolety = true;
+            if (vek <18)
+            {
+                plnolety = false;
+            }
+        }
+
+        public override string ToString()
+        {
+            string jsemPlnolety = "jsem";
+            if (!plnolety)
+                jsemPlnolety = "nejsem";
+            string pohlavi = "muž";
+            if (!muz)
+                pohlavi = "žena";
+            return String.Format("Jsem {0}, {1}. Je mi {2} let a {3} plnoletý.", jmeno, pohlavi, vek, jsemPlnolety);
+        }
+    }
+}
